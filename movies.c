949815910434deb9f63b11bde232904c97758f88
Yannik Hellmuth <yannik@backmuth.de>
2020-04-28 14:47:44 +0200

    ADD NOT WORKING READ METHOD

--- movies.c
+++ movies.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#define END "!END!"
+#define TABLE char * * *
+#define PATH "./Movies.csv"
+
+
 int count(char * INPUT,char muster){
 	int c=0;
 	int i=0;
@@ -9,77 +15,118 @@ int count(char * INPUT,char muster){
 		}
 		i++;
 	}
-	printf("%c %d\n",muster,c);
 	return c;
 }
 
-char * * * fromCSV(char * INPUT){
-	printf("INIT FROMCSV\n");
-	int size = 2*2*8;
-	printf("SIZE: %d\n",size);
-	char * * * OUTPUT=malloc(sizeof(char * *)*2);
-	for(int i=0;i<2;i++){
-		OUTPUT[i]=malloc(sizeof(char * *)*2);
+TABLE fromCSV(char * INPUT){
+	int L=count(INPUT,'\n');
+	int x=L+2;
+	int C=count(INPUT,',');
+	int y=(C+x-1)/(x-1);
+	printf("x: %d\ty: %d\n",x,y);
+	TABLE OUTPUT=malloc(sizeof(TABLE)*x);
+	for(int i=0;i<x;i++){
+		OUTPUT[i]=malloc(sizeof(char * *)*y);
 	}
-
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
+	for(int i=0;i<x;i++){
+		for(int j=0;j<y;j++){
 			OUTPUT[i][j]=malloc(sizeof(char *));
 		}
 	}
-	if(OUTPUT==0) printf("WHY\n"); else printf("OK\n");
 	int i=-1;
 	int a=0;
 	int b=0;
 	int c=0;
 	_Bool outside = 1;
 	char tmp;
-	printf("ADDR: 0x%010x\n",*OUTPUT);
-	printf("MAIN LOOP FROMCSV\n");
 	while(INPUT[i+1]!=0){
 		i++;
-		printf("%d\n",i);
 		if(INPUT[i]=='"'){
 			outside=!outside;
 			continue;
 		}
-		printf("outside\n");
 		if(!outside){
-			printf("%d, %d, %d\n",a,b,c);
 			tmp=INPUT[i];
 			OUTPUT[a][b][c]=tmp;
 			c++;
 		}
-		printf("outside end\n");
 		if(outside){
-			printf("1\n");
 			OUTPUT[a][b][c]=0;
-			printf("1\n");
 			if(INPUT[i]==','){
 				b++;
 			}
-			printf("2\n");
 			if(INPUT[i]=='\n'){
 				b=0;
 				a++;
 			}
-			printf("3\n");
 			c=0;
 		}
 	}
+	i++;
+	OUTPUT[x-1][0]=END;
 
-	printf("END FROMCSV\n");
 	return OUTPUT;
 }
 
-int main(){
-	char *** TEST=fromCSV("\"HALLO\",\"WELT\"\n\"Wie\",\"gehts\"");
-	for(int i=0;i<2;i++){
-		for(int j=0;j<2;j++){
-			printf("%s\t",TEST[i][j]);
+int CSVstrLen(TABLE INPUT){
+	int length=0;
+	for(int i=0;strcmp(INPUT[i][0],END);i++){
+		for(int j=0;j<3;j++){
+			length=strlen(INPUT[i][j])+3;
 		}
-		printf("\n");
+		length=length+1;
 	}
+	return length;
+}
+char * toCSV(TABLE INPUT){
+	int c=0;
+	int k=0;
+	int l=CSVstrLen(INPUT);
+	char * OUTPUT = malloc(l*sizeof(char *));
+	for(int i=0;strcmp(INPUT[i][0],END);i++){
+		for(int j=0;j<3;j++){
+			k=0;
+			if(j>0){
+				OUTPUT[c]=',';
+				c++;
+			}
+			OUTPUT[c]='"';
+			c++;
+			while(INPUT[i][j][k]!=0){
+				OUTPUT[c]=INPUT[i][j][k];
+				c++;
+				k++;
+			}
+			OUTPUT[c]='"';
+			c++;
+			k++;
+		}
+		OUTPUT[c]='\n';
+		c++;
+	}
+	return OUTPUT;
+}
+
+TABLE read(char * path){
+	FILE *f = fopen(path,"r");
+	if(f==NULL){
+		fputs("OPEN FILE FAILED\n", stderr);
+		return NULL;
+	}
+	fseek(f, 0L, SEEK_END);
+	int sz = ftell(f);
+	rewind(f);
+	char * content = malloc(sz*sizeof(char *));
+	while(fgets(content,sz,f)!=NULL);
+	return fromCSV(content);
+}
+
+
+
+int main(){
+	char *** TEST=read(PATH);
+	char * Test2=toCSV(TEST);
+	printf("%s",Test2);
 	return 0;
 
 
